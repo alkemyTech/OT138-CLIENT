9ba59c0f91888c931042712e033fd2b8e2f11c37
Facundo Juarez <facundojuarezf@outlook.com>
2022-02-17 20:27:05 -0300

    Add console warnings in case env variables are not defined

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -1,9 +1,21 @@
 /**
  * This file exports an instance of axios configured to make requests to the backend.
+ * The fi
  * Usage example: api.get('/users/1');
  */
 import axios from 'axios';
 
+// Notify in case required env variables are not declared in environment file
+(() => {
+  if(typeof process.env.REACT_APP_API_URL === 'undefined') {
+    console.warn('Variable REACT_APP_API_URL not defined in environment file');
+  }
+  if(typeof process.env.REACT_APP_LS_TOKEN === 'undefined') {
+    console.warn('Variable REACT_APP_LS_TOKEN not defined in environment file');
+  }
+})();
+
+// Create axios instance
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
@@ -14,8 +26,7 @@ const api = axios.create({
 
 //Add the JWT token to the Authorization header on each request
 api.interceptors.request.use((config) => {
-  const lsTokenKey = process.env.REACT_APP_LS_TOKEN || 'token';
-  const bearerToken = localStorage.getItem(lsTokenKey);
+  const bearerToken = localStorage.getItem(process.env.REACT_APP_LS_TOKEN);
 
   if(bearerToken) {
     config.headers.Authorization = `Bearer ${bearerToken}`;
