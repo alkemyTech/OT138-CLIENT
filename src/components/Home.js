b2511d86bbbe5bab3e23b5e34fff2ab79a9313cc
Agustín Tornielli <agustint@live.com>
2022-02-18 12:37:43 -0300

    Checking if ci/cd is working

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
-            <p>This is a Home Page</p>
+            <p>This is a Home Page :)</p>
         </div>
     )
 };
