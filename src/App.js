8006b7eca36b592e14fb42fdebcfff6ac7e85df8
agustints <agustint@live.com>
2022-02-18 16:29:10 -0300

    Adding login and register render route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Counter from './components/Counter';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Counter from "./components/Counter";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/registro" element={<Signup />} />
           <Route exact path="/counter" element={<Counter />} />
         </Routes>
       </Router>
