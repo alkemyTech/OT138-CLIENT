35e50917f809b8ddf808780cfb290cc57f51ccbc
Yosip Mike Colin <colinparrado@gmail.com>
2022-02-18 13:21:38 -0500

    Sections Complete

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,9 +1,11 @@
-import React, {useState } from 'react';
+import React, {Fragment, useState } from 'react';
 import styled from 'styled-components';
 import FondoPortada1 from "../Img/portada-1.png";
 import FondoPortada2 from "../Img/portada-2.jpg";
 import FondoPortada3 from "../Img/portada-3.jpg";
 import FondoPortada4 from "../Img/portada-4.jpg";
+import ImagenSecion1 from "../Img/secion-1.png";
+import ImagenSecion2 from "../Img/secion-2.png";
 import 'animate.css';
 
 function Home(){
@@ -99,7 +101,7 @@ function Home(){
 
 
   return (
-  
+  <Fragment>
   <Container>
       <Portada fondo={slider[position].imagen}>
         <h1 className={classAnimation}>{slider[position].titulo}</h1>
@@ -114,9 +116,32 @@ function Home(){
      </CajaItems>
       </Portada>
 
-
-     
   </Container>
+
+  <Secion1>
+  <Caja1>
+    <h1>Build an Interative Comapny Directory</h1>
+    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem.</p>
+    <button>Conocer</button>
+  </Caja1>
+ <Caja2>
+   <img src={ImagenSecion1} alt="secion1"width="70%" />
+ </Caja2>
+  </Secion1>
+
+
+  <Secion2>
+  <Caja3>
+<img src={ImagenSecion2} alt="secion1"width="70%" />
+</Caja3>
+
+<Caja4>
+<h1>Build an Interative Comapny Directory</h1>
+    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem.</p>
+    <button>Conocer</button>
+</Caja4>
+  </Secion2>
+</Fragment>
   );
 }
 
@@ -194,7 +219,7 @@ const Boton = styled.button`
 
   padding:20px 60px;
   margin:50px;
-  background:#8A39E1;
+  background:#efcc70;
   font-family: 'Poppins', sans-serif;
   font-size:20px;
   border:none;
@@ -237,6 +262,203 @@ bottom:50px;
 @media (max-width:500px){
 bottom:15px;
 }
+`;
+
+
+const Secion1 = styled.section`
+width:100%;
+height:auto;
+margin-top:100px;
+display:grid;
+grid-template-columns: 1fr;
+grid-template-rows:auto;
+grid-template-areas: "caja1 caja2";
+
+
+
+
+@media (max-width:2500px){
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: "caja1 caja2";
+}
+
+
+@media (max-width:500px){
+  grid-template-columns: 1fr;
+  grid-template-areas: "caja1" "caja2";
+  margin-top:50px;
+}
+
+
+@media (max-width:1359px){
+  grid-template-columns: 1fr;
+  grid-template-areas: "caja1" "caja2";
+}
+`;
+
+
+
+const Caja1 = styled.section`
+
+font-family: 'Poppins', sans-serif;
+padding:40px;
+grid-area:caja1;
+
+h1{
+font-weight:400;
+font-size:60px;
+}
+
+button{
+  border:none;
+  padding:15px 60px;
+  font-size:17px;
+  border-radius:5px;
+  cursor:pointer;
+  background-color:#efcc70;
+  margin:10px 0px;
+  
+
+}
+
+@media (max-width:2500px){
+  padding-left:150px;
+}
+
+@media (max-width:1559px){
+  padding-left:150px;
+
+  h1{
+    font-size:50px;
+  }}
+
+  @media (max-width:1359px){
+
+    text-align:center;
+    padding:10px;
+
+  }
+
+@media (max-width:500px){
+  text-align:center;
+  padding:10px;
+ 
+  h1{
+    font-size:30px;
+    width:100%;
+    margin:20px 0px;
+  }}
+`;
+
+
+const Caja2 = styled.section`
+
+padding:20px;
+display:flex;
+grid-area:caja2;
+justify-content:center;
+align-items:center;
+
+img{
+  border-radius:10px;
+}
+`;
+
+const Secion2 = styled.section`
+width:100%;
+height:auto;
+margin-top:100px;
+display:grid;
+grid-template-columns: 1fr;
+grid-template-rows:auto;
+grid-template-areas: "caja3 caja4";
+
+
+
+
+@media (max-width:2500px){
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: "caja3 caja4";
+}
+
+
+@media (max-width:500px){
+  grid-template-columns: 1fr;
+  grid-template-areas: "caja4" "caja3";
+  margin-top:50px;
+}
+
+
+@media (max-width:1359px){
+  grid-template-columns: 1fr;
+  grid-template-areas: "caja4" "caja3";
+}
+`;
+
+const Caja3 = styled.div`
+
+grid-area:caja3;
+padding:20px;
+display:flex;
+justify-content:center;
+align-items:center;
+
+img{
+  border-radius:10px;
+}
+
+`;
+
+
+const Caja4 = styled.div`
+
+grid-area:caja4;
+padding:40px;
+
+h1{
+font-weight:400;
+font-size:60px;
+}
+
+button{
+  border:none;
+  padding:15px 60px;
+  font-size:17px;
+  border-radius:5px;
+  cursor:pointer;
+  background-color:#efcc70;
+  margin:10px 0px;
+  
+
+}
+
+@media (max-width:2500px){
+  padding-right:150px;
+}
+
+
+
+@media (max-width:1559px){
+  padding-right:150px;
+h1{
+font-size:50px;
+}}
+
+@media (max-width:1359px){
+text-align:center;
+padding:10px;
+}
+  
+@media (max-width:500px){
+  text-align:center;
+  padding:10px;
+ 
+  h1{
+    font-size:30px;
+    width:100%;
+    margin:20px 0px;
+  }}
+
 `;
 
 
