db6c2169003420d137393318d2174ca1b5ad8d3e
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-02-17 21:40:54 -0300

    create styled component authHeader
    create header component to be used on login and sign up form views. The image is temporary. The styles needs to be unified with the rest of the api

---
+++ src/components/authHeader.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function AuthHeader(props){
+
+    const Header = styled.header`
+        width: 100%;
+        padding: 20px;
+        box-sizing: border-box;
+    `;
+
+    const Brand = styled.div`
+        padding: 20px;
+        box-sizing: border-box;
+    `;
+
+    const Divisor = styled.hr`
+        width: 90%;
+        border: solid 1px #BBAE93;
+    `
+
+    const Subtitle = styled.h2`
+        padding: 20px;
+        box-sizing: border-box;
+        text-align: center;
+        color: #17A768;
+    `
+
+    return(
+        <Header>
+            <Brand><img src={props.logoSrc} alt="site logo"/></Brand>
+            <Divisor/>
+            <Subtitle>{props.subtitle}</Subtitle>
+        </Header>
+    )
+}
+
+export default AuthHeader;
