465a5e2e259d3453cb153c800fb79be27881de42
agustints <agustint@live.com>
2022-02-19 17:07:59 -0300

    Login Styles fix

--- src/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-/* IMPORT REDUCERS */
-import exampleReducer from './exampleReducer';
-
-export default combineReducers({
-    exampleStore: exampleReducer,
-});
