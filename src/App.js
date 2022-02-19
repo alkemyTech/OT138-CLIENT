bc86784c8c2d5257d49cbf7a54a0bfc01d145371
agustints <agustint@live.com>
2022-02-19 11:29:41 -0300

    Squashed commit of the following:
    commit 5aa69a246df27185e1552a140fa2060cc14c8010
    Author: Yosip Mike Colin <colinparrado@gmail.com>
    Date:   Fri Feb 18 15:50:44 2022 -0500
    
        converting text from spanish to english
    
    commit 96a9fcd053c31aa58f5bd5705af8bde51376cc30
    Author: Yosip Mike Colin <colinparrado@gmail.com>
    Date:   Fri Feb 18 15:12:31 2022 -0500
    
        Menu create
    
    commit 35e50917f809b8ddf808780cfb290cc57f51ccbc
    Author: Yosip Mike Colin <colinparrado@gmail.com>
    Date:   Fri Feb 18 13:21:38 2022 -0500
    
        Sections Complete
    
    commit 4993b58d0752fa5a8aae9ee06e05dd154f226cfc
    Author: Yosip Mike Colin <colinparrado@gmail.com>
    Date:   Fri Feb 18 11:46:52 2022 -0500
    
        Slider complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,120 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Counter from "./components/Counter";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
+import { Link, NavLink } from "react-router-dom";
+import "./App.css";
+import styled from "styled-components";
+import Logo from "./Img/logo.png";
+import IconBurger from "./Img/menu.png";
 
 function App() {
+  const [high, SetHigh] = useState("0px");
+  function MostrarMenu() {
+    SetHigh("100vh");
+  }
+  function OcultarMenu() {
+    SetHigh("0px");
+  }
+
   return (
-    <Router>
+    <Fragment>
+      <header>
+        <Menu>
+          <Link to="/">
+            <img src={Logo} width="60px" alt="logo" />
+          </Link>
+          <ListMenu>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/signup">Signup</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </ListMenu>
+          <IconMenu src={IconBurger} alt="menu" onClick={MostrarMenu} />
+        </Menu>
+
+        <MenuMobil alto={high} onClick={OcultarMenu}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/signup">Signup</NavLink>
+          <NavLink to="/login">Login</NavLink>
+        </MenuMobil>
+      </header>
+
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/registro" element={<Signup />} />
-        <Route exact path="/counter" element={<Counter />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<h1>services</h1>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </Fragment>
   );
 }
 
+const Menu = styled.nav`
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  width: 100%;
+  -webkit-box-shadow: 0px 3px 7px -1px #000000;
+  box-shadow: 0px 3px 7px -1px #000000;
+`;
+
+const MenuMobil = styled.nav`
+  width: 100%;
+  background: #323232;
+  position: absolute;
+  z-index: 3;
+  top: 0;
+  height: ${(props) => props.alto};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  transition: 500ms ease;
+
+  a {
+    font-size: 60px;
+    margin: 20px 0px;
+    color: #fff;
+    transition: 500ms ease;
+  }
+
+  a.active {
+    color: #270082;
+  }
+`;
+
+const ListMenu = styled.div`
+  a {
+    margin: 0px 20px;
+    font-size: 18px;
+    color: #3b2284;
+    transition: 500ms ease;
+    border-bottom: 2px solid #fff;
+  }
+
+  a.active {
+    color: #5800ff;
+    border-bottom: 2px solid #5800ff;
+  }
+
+  @media (max-width: 778px) {
+    display: none;
+  }
+`;
+
+const IconMenu = styled.img`
+  width: 50px;
+  display: none;
+  cursor: pointer;
+  @media (max-width: 778px) {
+    display: block;
+  }
+`;
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
-import store from "../src/store/index";
+import store from "./app/store";
 import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
-import "./styles/index.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
