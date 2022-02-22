f68835d22d40b5de58b2a981988f0ee8cb511c89
Agustin-Q <agustin.queija@gmail.com>
2022-02-21 23:43:29 -0300

    Add documentation

--- src/services/requests/publicData.js
+++ src/services/requests/publicData.js
@@ -1,6 +1,13 @@
 import api from "../../config/api";
 import { API_PUBLIC_DATA } from "../../constants/urls";
 
+/**
+ * Makes a GET request to the API's Public Organization Data endpoint.
+ * @returns An object with the following entries:
+ *      - success: whether or not the login attempt was successful
+ *      - data: user data returned from the API, if success is true
+ *      - errorResponse: a string with the error message, if success is false
+ */
 export async function getPublicData() {
   const result = {
     success: false,
