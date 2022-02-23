50c0a2b31554cd761443e0f9249c4344194dd935
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-02-22 21:35:39 -0300

    improve styles and components

--- src/styles/Profile/index.js
+++ src/styles/Profile/index.js
@@ -2,11 +2,23 @@ import styled from "@emotion/styled";
 
 export const ProfileContainer = styled.section`
     width: 100%;
-    max-width: 800px;
+`;
+
+export const ProfileContent = styled.div`
+    width: 100%;
     padding: 50px;
     box-sizing: border-box;
-    background-color: grey;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
 `;
+
+export const Message = styled.div`
+    width: 100%;
+    text-align: center;
+    font-size: 2rem;
+`
 
 export const Field = styled.div`
     width: 100%;
@@ -27,6 +39,43 @@ export const Field = styled.div`
         padding: 10px;
         box-sizing: border-box;
         border-radius: 8px;
-        border: 1px solid rgba(0,0,0,0.6);
+        border: none;
+        background-color: #edf2f7;
     }
+    input:disabled{
+        background-color: rgba(0,0,0,0.01);
+    }
+`;
+
+export const Button = styled.button`
+    width: 200px;
+    padding: 10px;
+    margin-top: 10px;
+    border-radius: 10px;
+    box-sizing: border-box;
+    background-color: #3b2284;
+    color: white;
+    border: none;
+`;
+
+export const Image = styled.div`
+    width: 200px;
+    height: 200px;
+    border: 3px solid #3b2284;
+    margin: 20px;
+    border-radius: 100px;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 100%;
+        display: block;
+    }
+`;
+
+export const TextFields = styled.div`
+    flex: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
