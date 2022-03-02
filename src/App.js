26c76aea3baa98e10b4ae268f2ec94229505cec1
Yosip Mike Colin <colinparrado@gmail.com>
2022-03-01 19:22:15 -0500

    Full novelty detail-2

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import NewsID from "./views/NewsID";
 function App({ checkAuthentication }) {
   useEffect(() => {
     checkAuthentication();
-  }, []);
+  }, [checkAuthentication]);
 
   return (
     <>
