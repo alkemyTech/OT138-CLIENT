fe582d083d1c1cb3df9493140b10a5685e2a044f
Agustin-Q <agustin.queija@gmail.com>
2022-03-02 19:23:50 -0300

    Extract styles to external file

--- src/views/Form_contact.js
+++ src/views/Form_contact.js
@@ -1,114 +1,17 @@
 import React, { useState } from "react";
-import styled from "@emotion/styled";
+import {
+  Container,
+  ContainColumn,
+  Colum,
+  Form,
+  Input,
+  Textarea,
+  Button,
+  MessageError,
+} from "../styles/Contacts";
 import { Formik } from "formik";
 import { createContact } from "../services/requests/contacts";
 import Alert from "../components/Alert";
-
-const Container = styled.section`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  background-color: #eeeeee;
-  padding: 20px;
-`;
-
-const ContainColumn = styled.div`
-  display: flex;
-  width: 60%;
-  height: 450px;
-  margin-top: 100px;
-
-  @media (max-width: 1298px) {
-    flex-direction: column;
-    margin-top: 0px;
-  }
-
-  @media (max-width: 800px) {
-    width: 100%;
-  }
-`;
-
-const Colum = styled.div`
-  padding: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  background-image: url("https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80");
-  background-size: cover;
-  border-radius: 10px 0px 0px 10px;
-  width: 50%;
-  h1 {
-    color: #fff;
-    font-size: 50px;
-  }
-
-  @media (max-width: 1298px) {
-    width: 100%;
-    border-radius: 10px 10px 0px 0px;
-  }
-`;
-
-const Form = styled.form`
-  padding: 30px;
-  width: 50%;
-  border-radius: 0px 10px 10px 0px;
-  background-color: #fff;
-  @media (max-width: 1298px) {
-    width: 100%;
-    border-radius: 0px 0px 10px 10px;
-  }
-`;
-
-const Input = styled.input`
-  border: none;
-  display: block;
-  width: 100%;
-  padding: 15px;
-  margin-top: 20px;
-  outline: none;
-  border-radius: 5px;
-  background-color: #e6e6e7;
-  font-size: 15px;
-`;
-
-const Textarea = styled.input`
-  border: none;
-  display: block;
-  padding: 40px;
-  margin-top: 20px;
-  width: 100%;
-  outline: none;
-  background-color: #e6e6e7;
-  border-radius: 5px;
-  font-size: 18px;
-`;
-
-const Button = styled.button`
-  border: none;
-  background-color: #116530;
-  color: #fff;
-  padding: 10px 20px;
-  font-size: 15px;
-  border-radius: 5px;
-  margin-top: 20px;
-  cursor: pointer;
-  font-family: "Open Sans", sans-serif;
-  transition: 500ms ease;
-  &:hover {
-    background-color: #357c3c;
-  }
-`;
-
-const MessageError = styled.span`
-  color: #da1212;
-  font-size: 12px;
-  margin-left: 5px;
-  font-family: "Open Sans", sans-serif;
-  font-weight: bold;
-  display: block;
-`;
 
 function FormContacto() {
   const [alert, setAlert] = useState({});
