d9005bddf7bd147f87241ccbb6a878ff12813d61
Facundo Juarez <facu.j.94@gmail.com>
2022-03-02 15:47:17 -0300

    Create Table styled component

--- src/views/BackOffice/Categories.js
+++ src/views/BackOffice/Categories.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Table from "../../components/Table";
 
 export default function Categories() {
     return (
         <div>
-            
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Fecha creación</th>
+                        <th>Fecha actualización</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
