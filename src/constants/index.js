a45679d1f13074168539ec313ed62f158e14dc12
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-03 15:18:52 -0300

    Add api errors mapping (#34)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,18 @@
 // Enum representing a request status
-export const status = {
+export const status = Object.freeze({
     PENDING: 'PENDING',
     FETCHING: 'FETCHING',
     SUCCESS: 'SUCCESS',
     FAILURE: 'FAILURE'
-}
+});
+
+// Mapping of api error codes to messages
+export const apiErrors = Object.freeze({
+    AUT001: 'Autenticación requerida',
+    AUT002: 'Acceso no permitido',
+    AUT003: 'Sesión expirada',
+    VAL001: 'Error de validación',
+    REQ001: 'Recurso no encontrado',
+    REQ002: 'Solicitud inválida',
+    SRV001: 'Error del servidor'
+});
