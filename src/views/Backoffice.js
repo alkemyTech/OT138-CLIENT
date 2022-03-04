fc6612305589b6e5f25a0f1279a5af36416a228a
agustints <agustint@live.com>
2022-03-04 18:26:54 -0300

    Merge

--- src/views/Backoffice.js
+++ src/views/Backoffice.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import Header from "../components/Header/BackOffice";
 import { Route, Routes } from "react-router-dom";
 import { Container } from "../components/Wrappers/Containers";
-import Root from './BackOffice/Root';
-import Categories from './BackOffice/Categories';
-import Contacts from './BackOffice/Contacts';
-import Users from './BackOffice/Users';
+import Root from "./BackOffice/Root";
+import Categories from "./BackOffice/Categories";
+import Contacts from "./BackOffice/Contacts";
+import Users from "./BackOffice/Users";
 
 export default function Backoffice() {
-    return (
-        <Container>
-            <Header />
-            <Toaster/>
-            <Routes>
-                <Route path="/" element={<Root />} />
-                <Route path="/categories" element={<Categories />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/users" element={<Users />} />
-            </Routes>
-        </Container>
-    );
+  return (
+    <Container>
+      <Header />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Container>
+  );
 }
