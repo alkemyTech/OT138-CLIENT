6959e74859cae069a7d11f71a1933fec9e7be528
agustints <agustint@live.com>
2022-03-04 12:02:00 -0300

    Minor fix

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,7 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 /* IMPORT REDUCERS */
-import authReducer from './authReducer';
+import authReducer from "./authReducer";
 
 export default combineReducers({
-    auth: authReducer
+  auth: authReducer,
 });
