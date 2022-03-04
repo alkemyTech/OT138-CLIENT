f03b2849a266a286d5e364555ba624b380a1ce67
Facundo Juarez <facu.j.94@gmail.com>
2022-03-04 13:26:14 -0300

    Add getContacts service

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,3 +4,4 @@ export const API_AUTH_GET_PROFILE_DATA = "/auth/me";
 export const API_PUBLIC_DATA = "/organizations/1/public";
 export const API_AUTH_CHECK = "/auth/imloggedin";
 export const API_CATEGORIES =  "/categories";
+export const API_CONTACTS = "/contacts";
