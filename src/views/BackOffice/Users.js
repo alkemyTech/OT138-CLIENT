7f85b23bb09d51bb4d46894eb809298aa197cb37
Facundo Juarez <facu.j.94@gmail.com>
2022-03-04 16:40:39 -0300

    Update Users table component

--- src/views/BackOffice/Users.js
+++ src/views/BackOffice/Users.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Table from "../../components/Table";
 import { getUsers } from '../../services/requests/users';
-import { CategoriesContainer as UsersContainer } from '../../styles/Categories';
+import { SectionWrapper as UsersContainer } from '../../styles/BackOffice';
 import { Button, ButtonGroup } from "../../components/Inputs";
 import toast from 'react-hot-toast';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 
-export default function Users () {
+export default function Users() {
     const [users, setUsers] = useState([]);
 
     async function fetchUsers() {
         const { success, data: users, errorMessage } = await getUsers();
-        console.log(JSON.stringify(await getUsers()));
+        //console.log(JSON.stringify(await getUsers()));
 
         if (success) {
             setUsers(users);
@@ -30,39 +30,39 @@ export default function Users () {
             height: '40px',
             background: color,
         }
-    } 
+    }
 
     return (
         <UsersContainer>
             <h2>Usuarios</h2>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Email</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.length > 0 ? users.map((user, i) => {
-                        return (
-                            <tr key={i}>
-                                <td>{user.firstName}</td>
-                                <td>{user.lastName}</td>
-                                <td>{user.email}</td>
-                                <td>
-                                    <ButtonGroup alignEnd>
-                                        <Button style={buttonStyles('orange')}><FaEdit /></Button>
-                                        <Button style={buttonStyles('red')}><FaTrash /></Button>
-                                    </ButtonGroup>
-                                </td>
-                            </tr>
-                            )
-                        }) : null
+            <Table
+                headers={['Nombre', 'Apellido', 'Email', '']}
+                data={users.map(user => {
+                    return {
+                        ...user,
+                        buttons: (
+                            <ButtonGroup alignEnd>
+                                <Button style={buttonStyles('orange')}><FaEdit /></Button>
+                                <Button style={buttonStyles('red')}><FaTrash /></Button>
+                            </ButtonGroup>
+                        )
                     }
-                </tbody>
-            </Table>
+                })}
+                accessors={[
+                    {
+                        name: 'firstName'
+                    },
+                    {
+                        name: 'lastName'
+                    },
+                    {
+                        name: 'email'
+                    },
+                    {
+                        name: 'buttons'
+                    }
+                ]}
+            />
         </UsersContainer>
     )
 };
