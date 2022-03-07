8384be043ee8e3556d7d9360e3d3ca98d398be94
Facundo Juarez <facu.j.94@gmail.com>
2022-03-07 20:59:44 -0300

    Add Form component docs

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
-import {
-  Button,
-  ButtonGroup,
-  Input,
-  Label,
-  TextArea,
-} from "../../components/Inputs";
+import { ButtonGroup, Input, Label } from "../../components/Inputs";
 import { FormStyle, CancelButton, SubmitButton, InputFeedback } from "./styles";
 
+/**
+ * Generic form component. Renders an update form or a create form depending on the value of the instance prop.
+ * @param {*} props Properties:
+ *      - fields: (required) Array of objects containing the form inputs with the format: {name, type, placeholder}.
+ *             Example: [{name: 'description', type: 'text', placeholder: 'Enter description'}, ...]
+ *      - instance: (optional). Object containing the data of the instance. If defined, inputs will be filled with the
+ *          data of the instance object, accessed by the 'name' propery in the fields array.
+ *      - onSubmit: (required) Callback function to be executed after clicking the submit button
+ *      - onCancel: (required) Callback function to be executed after clicking the cancel button
+ *      - errors: (optional) Object containing errors to be displayed under the inputs. It should contain a key-value pair in the form:
+ *        { fieldName: errorText }
+ */
 export default function Form({
   fields = [],
   instance,
