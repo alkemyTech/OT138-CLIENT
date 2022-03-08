f70233dea90b76a8e03ee9d768a208d28dd06453
Yosip Mike Colin <colinparrado@gmail.com>
2022-03-08 18:14:08 -0500

    Applying changes from the dev branch

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -5,9 +5,4 @@ export const API_PUBLIC_DATA = "/organizations/1/public";
 export const API_AUTH_CHECK = "/auth/imloggedin";
 export const API_CATEGORIES =  "/categories";
 export const API_CONTACTS = "/contacts";
-<<<<<<< HEAD
 export const API_USERS = "/users";
-export const API_TESTIMONIAL = "testimonials/";
-=======
-export const API_USERS = "/users";
->>>>>>> 8e46048e7c7c2d1940e87c93aca4228ad6fb17db
