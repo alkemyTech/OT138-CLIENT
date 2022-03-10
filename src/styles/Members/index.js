6f12c7d8c384fff2e7f03f3ba8b7fdb683bacb79
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-10 20:10:35 -0300

    Add styles for members view

---
+++ src/styles/Members/index.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const MembersContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 100%;
+    flex: 1;
+    padding: 1.5rem 0;
+`;
