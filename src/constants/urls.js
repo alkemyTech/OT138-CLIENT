f403fdff205f709b11ad68a1eae02df71fa6849c
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-11 11:43:37 -0300

    Refactor auth routes response format (#60)
    * Refactor signup register
    
    * Refactor login view
    
    * Fix axios interceptor

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,4 @@
+export const API_AUTH_REGISTER = "/auth/register";
 export const API_AUTH_LOGIN = "/auth/login";
 export const API_AUTH_LOGOUT = "/auth/logout";
 export const API_AUTH_REFRESH = "/auth/refresh";
