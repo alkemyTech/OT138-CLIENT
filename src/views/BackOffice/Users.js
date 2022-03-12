8f955a673270e4aaf23cdd13ba850a83c7ad0cb7
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-12 11:10:52 -0300

    Refactor table component (#62)
    * Update category table usage
    
    * Update contact table usage
    
    * Update users table usage

--- src/views/BackOffice/Users.js
+++ src/views/BackOffice/Users.js
@@ -7,78 +7,79 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 import { Content } from "../../components/Wrappers/Containers";
 
 export default function Users() {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-  async function fetchUsers() {
-    const { success, data: users, errorMessage } = await getUsers();
-    //console.log(JSON.stringify(await getUsers()));
+    async function fetchUsers() {
+        const { success, data: users, errorMessage } = await getUsers();
+        //console.log(JSON.stringify(await getUsers()));
 
-    if (success) {
-      setUsers(users);
-    } else {
-      toast.error(`Error fetching users: ${errorMessage}`);
+        if (success) {
+            setUsers(users);
+        } else {
+            toast.error(`Error fetching users: ${errorMessage}`);
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-  function buttonStyles(color) {
-    return {
-      width: "40px",
-      height: "40px",
-      background: color,
-    };
-  }
+    function buttonStyles(color) {
+        return {
+            width: "40px",
+            height: "40px",
+            background: color,
+        };
+    }
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-  function buttonStyles(color) {
-    return {
-      width: "40px",
-      height: "40px",
-      background: color,
-    };
-  }
+    function buttonStyles(color) {
+        return {
+            width: "40px",
+            height: "40px",
+            background: color,
+        };
+    }
 
-  return (
-    <Content>
-      <h2>Usuarios</h2>
-      <Table
-        headers={["Nombre", "Apellido", "Email", ""]}
-        data={users.map((user) => {
-          return {
-            ...user,
-            buttons: (
-              <ButtonGroup alignEnd>
-                <Button style={buttonStyles("orange")}>
-                  <FaEdit />
-                </Button>
-                <Button style={buttonStyles("red")}>
-                  <FaTrash />
-                </Button>
-              </ButtonGroup>
-            ),
-          };
-        })}
-        accessors={[
-          {
-            name: "firstName",
-          },
-          {
-            name: "lastName",
-          },
-          {
-            name: "email",
-          },
-          {
-            name: "buttons",
-          },
-        ]}
-      />
-    </Content>
-  );
+    return (
+        <Content>
+            <h2>Usuarios</h2>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Email</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{user.firstName}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <ButtonGroup alignEnd>
+                                            <Button style={buttonStyles("orange")}>
+                                                <FaEdit />
+                                            </Button>
+                                            <Button style={buttonStyles("red")}>
+                                                <FaTrash />
+                                            </Button>
+                                        </ButtonGroup>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+        </Content>
+    );
 }
