32958b90d82be4a3a12f82c75824d1b36bca84f3
Facundo Juarez <facundojuarezf@outlook.com>
2022-03-12 10:24:08 -0300

    Refactor Activities view

--- src/views/Activities.js
+++ src/views/Activities.js
@@ -11,129 +11,120 @@ import Header from "../components/Header/Landing";
 import Loading from "../components/Loading";
 import { Container, Content } from "../components/Wrappers/Containers";
 import {
-  getActivities,
-  getActivitiesById,
+    getActivities,
+    getActivitiesById,
 } from "../services/requests/activities";
 import { ActivitiesContainer } from "../styles/Activities";
 import Banner from "../components/Banner";
 
 
 export default function Activities() {
-  let totalPages;
-  const [currentPage, setCurrentPage] = useState(1);
-  const limit = 15;
-  const {
-    data: activities,
-    isLoading,
-    isFetching,
-    isError,
-    refetch,
-  } = useQuery(
-    ["activities", limit, currentPage],
-    () => getActivities(limit, currentPage),
-    {
-      retry: false,
-    }
-  );
-  useEffect(() => {
-    refetch();
-  }, [currentPage]);
+    let totalPages;
+    const [currentPage, setCurrentPage] = useState(1);
+    const limit = 15;
 
-  totalPages = Math.ceil(activities?.data?.result?.count / limit);
-  return (
-    <Container>
-      <Header />
-      <Content>
-        <Banner
-          title={"Lista de Actividades"}
-          thumbnail={"/activities__banner.jpg"}
-        />
-        <ActivitiesContainer>
-          {!isLoading ? (
-            !activities?.data?.error && !isError ? (
-              activities?.data?.result?.activities?.map((activity) => {
-                const { id, name, image, content } = activity;
-                return (
-                  <Activity
-                    key={id}
-                    id={id}
-                    name={name}
-                    content={content}
-                    image={image}
-                  />
-                );
-              })
-            ) : (
-              <h1>¡En este momento no contamos con Actividades!</h1>
-            )
-          ) : !isLoading &&
-            !isFetching &&
-            activities?.data?.error &&
-            isError ? (
-            <h1>¡No se encontraron Actividades!</h1>
-          ) : (
-            <Loading />
-          )}
-        </ActivitiesContainer>
-        {!isLoading && (
-          <div className="divider">
-            <Pagination
-              current={currentPage}
-              total={totalPages}
-              onPageChange={setCurrentPage}
-              style={{ justifySelf: "end" }}
-            />
-          </div>
-        )}
-      </Content>
-      <Footer />
-    </Container>
-  );
+    const {
+        data: response,
+        isLoading,
+        isFetching,
+        isError,
+        refetch,
+    } = useQuery(
+        ["activities", limit, currentPage],
+        () => getActivities(limit, currentPage),
+        {
+            retry: false,
+        }
+    );
+
+    useEffect(() => {
+        refetch();
+    }, [currentPage]);
+
+    totalPages = Math.ceil(response?.data?.result?.count / limit);
+
+    return (
+        <Container>
+            <Header />
+            <Content>
+                <Banner
+                    title={"Lista de Actividades"}
+                    thumbnail={"/activities__banner.jpg"}
+                />
+                <ActivitiesContainer>
+                    {!isLoading ? (
+                        !response?.data?.error && !isError ? (
+                            response?.data?.result?.items?.map((activity) => {
+                                const { id, name, image, content } = activity;
+                                return (
+                                    <Activity
+                                        key={id}
+                                        id={id}
+                                        name={name}
+                                        content={content}
+                                        image={image}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <h1>¡En este momento no contamos con Actividades!</h1>
+                        )
+                    ) : !isLoading &&
+                        !isFetching &&
+                        response?.data?.error &&
+                        isError ? (
+                        <h1>¡No se encontraron Actividades!</h1>
+                    ) : (
+                        <Loading />
+                    )}
+                </ActivitiesContainer>
+                {!isLoading && (
+                    <div className="divider">
+                        <Pagination
+                            current={currentPage}
+                            total={totalPages}
+                            onPageChange={setCurrentPage}
+                            style={{ justifySelf: "end" }}
+                        />
+                    </div>
+                )}
+            </Content>
+            <Footer />
+        </Container>
+    );
 }
 
 export const ActivitiesByID = () => {
-  let { id } = useParams();
-  const navigate = useNavigate();
-  const {
-    data: activities,
-    isLoading,
-    isFetching,
-    isError,
-  } = useQuery(["activities-id", id], () => getActivitiesById(id));
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (
-          activities?.data &&
-          activities?.data?.result?.activities?.length <= 0
-        )
-          return navigate("/actividades");
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    let { id } = useParams();
+    const navigate = useNavigate();
 
-    fetchData();
-  }, [activities?.data]);
+    const {
+        data: response,
+        isLoading,
+        isFetching,
+        isError,
+    } = useQuery(["activities-id", id], () => getActivitiesById(id));
 
-  return (
-    <Container>
-      <Header />
-      <Content>
-        {activities?.data?.result?.activities?.map((activity) => {
-          const { id, name, image, content } = activity;
-          return (
-            <Article
-              key={id}
-              id={id}
-              title={name}
-              content={content}
-              thumbnail={image}
-            />
-          );
-        })}
-      </Content>
-      <Footer />
-    </Container>
-  );
+    return (
+        <Container>
+            <Header />
+            <Content>
+                {
+                    (isLoading || isFetching) ? <Loading /> :
+                        (!isError && !response.data.error)
+                            ?
+                            <Article
+                                key={response?.data?.result.id}
+                                id={response?.data?.result.id}
+                                title={response?.data?.result.title}
+                                content={response?.data?.result.content}
+                                thumbnail={response?.data?.result.image}
+                            />
+                            :
+                            <h3>Actividad no encontrada</h3>
+                }
+            </Content>
+            <Footer />
+        </Container>
+    );
 };
