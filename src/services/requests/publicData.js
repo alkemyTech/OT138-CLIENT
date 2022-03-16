a881ef75b5931296a929b85b53ff39784aba4424
Facundo Juarez <31292867+facundojuarez2k@users.noreply.github.com>
2022-03-16 13:09:38 -0300

    Fix edit organization view (#70)

--- src/services/requests/publicData.js
+++ src/services/requests/publicData.js
@@ -1,5 +1,6 @@
 import api from "../../config/api";
 import { API_PUBLIC_DATA } from "../../constants/urls";
+import { apiErrors } from '../../constants';
 
 /**
  * Makes a GET request to the API's Public Organization Data endpoint.
@@ -9,37 +10,44 @@ import { API_PUBLIC_DATA } from "../../constants/urls";
  *      - errorResponse: a string with the error message, if success is false
  */
 export async function getPublicData() {
-  const result = {
-    success: false,
-    data: {},
-    errorMessage: "",
-  };
-  try {
-    const { data: response } = await api.get(API_PUBLIC_DATA);
-    if (response.error) {
-      result.errorMessage = response.message;
-    } else {
-      result.success = true;
-      result.data = response;
+    const result = {
+        success: false,
+        data: {},
+        errorMessage: "",
+    };
+    try {
+        const { data: response } = await api.get(API_PUBLIC_DATA);
+        if (response.error) {
+            result.errorMessage = apiErrors[response.errorCode] ?? 'Error al actualizar datos públicos';
+            result.errorMessage += ": " + response.message;
+        } else {
+            result.success = true;
+            result.data = response.result;
+        }
+    } catch (error) {
+        result.errorMessage = "Error al obtener datos públicos";
     }
-  } catch (error) {
-    console.log(error);
-    result.errorMessage = "Found an unexpected error during the request";
-  }
-  return result;
+    return result;
 }
 
 export async function updatePublicData(organizationData) {
-  try {
-    const { data: response } = await api.put(API_PUBLIC_DATA, organizationData);
-    return response;
-  } catch (error) {
-    console.log(error);
-    return {
-      error: true,
-      errorCode: "SRV001",
-      status: "500",
-      message: `An unexpected error ocurred when storing data to the database. Details:  ${error.message}`,
+    const result = {
+        success: false,
+        data: {},
+        errorMessage: "",
     };
-  }
+    try {
+        const { data: response } = await api.put(API_PUBLIC_DATA, organizationData);
+        if (response.error) {
+            result.errorMessage = apiErrors[response.errorCode] ?? 'Error al actualizar';
+            result.errorMessage = ": " + response.message;
+        } else {
+            result.success = true;
+            result.data = response.result;
+        }
+    } catch (error) {
+        result.errorMessage = "Error al actualizar";
+    }
+
+    return result;
 }
