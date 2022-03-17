3f9e515b08e378dcbabf22fa01a64e0dd6e83c4a
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-17 18:51:20 -0300

    Add transitions wrapper

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import FormContacto from "./views/Contact";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./styles/App.css";
 import Backoffice from "./views/Backoffice";
 import Profile from "./views/Profile";
@@ -20,86 +20,94 @@ import EditForm from "./views/BackOffice/EditForm";
 import News, { NewsByID } from "./views/News";
 import TableTestimonial from "./views/BackOffice/Table_testimonial";
 import Testimonials from "./views/Testimonials";
+import { PageTransition } from '@steveeeie/react-page-transition';
 
 function App({ checkAuthentication }) {
   useEffect(() => {
     checkAuthentication();
   }, []);
 
+  const location = useLocation();
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<h1>Services</h1>} />
-        <Route path="/registro" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contacto" element={<FormContacto />} />
-        <Route path="/nosotros" element={<Members />} />
-        <Route path="/testimonios" element={<Testimonials />} />
-        <Route
-          path="/perfil"
-          element={
-            <Protected>
-              <Profile />
-            </Protected>
-          }
-        />
-        <Route path="/actividades" element={<Activities />} />
-        <Route path="/actividades/:id" element={<ActivitiesByID />} />
-        <Route path="/novedades" element={<News />} />
-        <Route path="/novedades/:id" element={<NewsByID />} />
-        <Route
-          path="/backoffice/*"
-          element={
-            <Protected isAdmin>
-              <Backoffice />
-            </Protected>
-          }
-        />
+      <PageTransition
+        preset="fadeFromRight"
+        transitionKey={location.pathname}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<h1>Services</h1>} />
+          <Route path="/registro" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contacto" element={<FormContacto />} />
+          <Route path="/nosotros" element={<Members />} />
+          <Route path="/testimonios" element={<Testimonials />} />
+          <Route
+            path="/perfil"
+            element={
+              <Protected>
+                <Profile />
+              </Protected>
+            }
+          />
+          <Route path="/actividades" element={<Activities />} />
+          <Route path="/actividades/:id" element={<ActivitiesByID />} />
+          <Route path="/novedades" element={<News />} />
+          <Route path="/novedades/:id" element={<NewsByID />} />
+          <Route
+            path="/backoffice/*"
+            element={
+              <Protected isAdmin>
+                <Backoffice />
+              </Protected>
+            }
+          />
 
-        <Route
-          path="/backoffice/testimonios"
-          element={
-            <Protected isAdmin>
-              <TableTestimonial />
-            </Protected>
-          }
-        />
+          <Route
+            path="/backoffice/testimonios"
+            element={
+              <Protected isAdmin>
+                <TableTestimonial />
+              </Protected>
+            }
+          />
 
-        <Route
-          path="/backoffice/actividades/nueva"
-          element={
-            <Protected isAdmin>
-              <ActivityEditor />
-            </Protected>
-          }
-        />
-        <Route
-          path="/backoffice/actividades/editar/:id"
-          element={
-            <Protected isAdmin>
-              <ActivityEditor />
-            </Protected>
-          }
-        />
-        <Route
-          path="/backoffice/usuarios/editar/:id"
-          element={
-            <Protected isAdmin>
-              <UserEditor />
-            </Protected>
-          }
-        />
+          <Route
+            path="/backoffice/actividades/nueva"
+            element={
+              <Protected isAdmin>
+                <ActivityEditor />
+              </Protected>
+            }
+          />
+          <Route
+            path="/backoffice/actividades/editar/:id"
+            element={
+              <Protected isAdmin>
+                <ActivityEditor />
+              </Protected>
+            }
+          />
+          <Route
+            path="/backoffice/usuarios/editar/:id"
+            element={
+              <Protected isAdmin>
+                <UserEditor />
+              </Protected>
+            }
+          />
 
-        <Route
-          path="/backoffice/slider"
-          element={
-            <Protected isAdmin>
-              <EditForm />
-            </Protected>
-          }
-        />
-      </Routes>
+          <Route
+            path="/backoffice/slider"
+            element={
+              <Protected isAdmin>
+                <EditForm />
+              </Protected>
+            }
+          />
+        </Routes>
+      </PageTransition>
     </>
   );
 }
