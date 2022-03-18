cd1d3bb32edd75137965a772b976287baf2e4277
agustints <agustint@live.com>
2022-03-18 20:37:24 -0300

    Merge

--- src/components/EntryEditor/index.js
+++ src/components/EntryEditor/index.js
@@ -25,14 +25,6 @@ function EntryEditor({ id, state, entryType, get, save, data, fields }) {
   useEffect(() => {
     setFieldsWithData(
       fields.map((field) => {
-        console.log({
-          ...field,
-          value: data[field.name]
-            ? data[field.name]
-            : field?.defaultValue
-            ? field?.defaultValue
-            : "",
-        });
         return {
           ...field,
           value: data[field.name]
