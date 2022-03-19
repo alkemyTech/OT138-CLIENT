b221c88700fa253cd91835df56c197c212198f1d
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-18 21:08:14 -0300

    Relocate Home component
    Move Home component from components/ to views/

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Container, Content } from "./Wrappers/Containers";
-import Slider from "./Slider";
+import { Container, Content } from "../components/Wrappers/Containers";
+import Slider from "../components/Slider";
 import Header from "../components/Header/Landing";
-import { Footer } from "./Footer";
-import Activities from "./Activities";
-import AboutUs from "./AboutUs";
-import News from "./News";
+import { Footer } from "../components/Footer";
+import Activities from "../components/Activities";
+import AboutUs from "../components/AboutUs";
+import News from "../components/News";
 function Home() {
   return (
     <Container>
