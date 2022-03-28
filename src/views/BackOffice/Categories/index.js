29fef0c5a24bdd5f542485cd19431d2538d6640b
Facundo Juarez <facundojuarezf@outlook.com>
2022-03-28 11:41:26 -0300

    Fix pagination

--- src/views/BackOffice/Categories/index.js
+++ src/views/BackOffice/Categories/index.js
@@ -197,7 +197,7 @@ export default function Categories() {
         {pagination && (
           <Pagination
             onPageChange={goToPage}
-            totalPages={pagination.pages || 0}
+            totalPages={pagination.pages || 1}
             forcePage={currentPage}
           />
         )}
