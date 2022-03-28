c0c5ec2317ec2d2dda115258906600877de5af13
Nicolás Oleinizak <78864158+nicolasoleinizak@users.noreply.github.com>
2022-03-28 12:04:37 -0300

    Members editor (#149)
    * Create Members Backoffice area
    
    * Update and create request services
    
    * Add members backoffice route to navBar
    
    * Add members editing functions and views

--- src/services/requests/members.js
+++ src/services/requests/members.js
@@ -1,8 +1,9 @@
 import api from "../../config/api";
 import { API_MEMBERS } from "../../constants/urls";
 import { apiErrors } from '../../constants';
+import { createFormData } from "../../helpers";
 
-export const getMembers = async () => {
+export const getMembers = async (page, pageLimit) => {
   const result = {
     success: false,
     errorMessage: '',
@@ -10,15 +11,102 @@ export const getMembers = async () => {
   };
 
   try {
-    const { data: resObj } = await api.get(API_MEMBERS);
+    const { data: resObj } = await api.get(API_MEMBERS, {
+      params: {
+        limit: pageLimit,
+        page: page
+      }
+    });
     if (resObj.error) {
       result.errorMessage = apiErrors[resObj.errorCode] ?? 'Error al obtener miembros';
+    } else {
+      result.success = true;
+      result.data = resObj.result;
+    }
+  } catch (err) {
+    result.errorMessage = 'Error al obtener miembros';
+  }
+  
+  return result;
+}
+
+export const deleteMember = async (id) => {
+  const result = {
+    success: false,
+    errorMessage: '',
+    data: []
+  };
+
+  try {
+    const { data: resObj } = await api.delete(`${API_MEMBERS}/${id}`);
+    if (resObj.error) {
+      result.errorMessage = apiErrors[resObj.errorCode] ?? 'Error al intentar eliminar';
     } else {
       result.success = true;
       result.data = resObj.data;
     }
   } catch (err) {
-    result.errorMessage = 'Error al obtener miembros';
+    result.errorMessage = 'Error al intentar eliminar';
+  }
+
+  return result;
+}
+
+export const createMember = async (data) => {
+  const result = {
+    success: false,
+    errorMessage: '',
+    data: []
+  };
+
+  try {
+    const { data: resObj } = await api.post(
+      API_MEMBERS,
+      createFormData(data),
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    );
+    if (resObj.error) {
+      result.errorMessage = apiErrors[resObj.errorCode] ?? 'Error al intentar crear el miembro';
+    } else {
+      result.success = true;
+      result.data = resObj.result;
+    }
+  } catch (err) {
+    result.errorMessage = 'Error al intentar crear el miembro';
+  }
+
+  return result;
+}
+
+export const updateMember = async (id, data) => {
+  const result = {
+    success: false,
+    errorMessage: '',
+    data: []
+  };
+
+  try {
+    const { data: resObj } = await api.put(
+      `${API_MEMBERS}/${id}`,
+      createFormData(data),
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    );
+    if (resObj.error) {
+      result.errorMessage = apiErrors[resObj.errorCode] ?? 'Error al intentar actualizar el miembro';
+    } else {
+      result.success = true;
+      result.data = resObj.data;
+    }
+  } catch (err) {
+    result.errorMessage = 'Error al intentar actualizar el miembro';
   }
 
   return result;
---
+++ src/views/BackOffice/Members/index.js
@@ -0,0 +1,249 @@
+import React, { useEffect, useState } from "react";
+import Table from "../../../components/Table";
+import moment from "moment";
+import {
+  getMembers as getAllMembersService,
+  deleteMember as deleteMemberService,
+} from "../../../services/requests/members";
+import toast from "react-hot-toast";
+import { Button, ButtonGroup } from "../../../components/Inputs";
+import { Content } from "../../../components/Wrappers/Containers";
+import Pagination, { SelectLimit } from "../../../components/Pagination";
+import {
+  FaEdit,
+  FaPlusSquare,
+  FaTrash,
+} from "react-icons/fa";
+import { TailSpin } from "react-loader-spinner";
+import Swal from "sweetalert2";
+import Modal from "../../../components/Modal";
+import { HeaderButtons, AddButton, SectionTitle } from "../../../styles/BackOffice";
+import MemberEditor from "./MemberEditor";
+import Skeleton from "react-loading-skeleton";
+import { AvatarSkeleton, AvatarWithSkeleton } from "../../../components/Skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
+import { createArrayOfObjects, removeTags } from "../../../helpers";
+
+function Members() {
+  const limitOptions = [10, 15, 25, 50];
+
+  const [pageLimit, setPageLimit] = useState(limitOptions[0]);
+  const [members, setMembers] = useState(createArrayOfObjects(pageLimit));
+  const [pagination, setPagination] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lockedEntryIds, setLockedEntryIds] = useState([]);
+  const [formData, setFormData] = useState({
+    display: false,
+    instance: null,
+  });
+  const [tableLoading, setTableLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch data to get new pagination after page limit change
+    getMembers(currentPage);
+  }, [pageLimit]);
+
+  useEffect(()=>{
+    // current page is grater than total pages, fetch data of last page
+    if (currentPage > pagination.pages) goToPage(pagination.pages);
+  },[pagination.pages])
+
+  async function getMembers(page) {
+    setTableLoading(true);
+
+    const {
+      success,
+      data: membersData,
+      errorMessage,
+    } = await getAllMembersService(page, pageLimit);
+
+    if (success) {
+      const { items, ...pagination } = membersData;
+      setMembers(items);
+      setPagination(pagination);
+    } else {
+      toast.error("Error al obtener miembros: " + errorMessage);
+    }
+    setTableLoading(false);
+
+  }
+
+  async function goToPage(page) {
+    setCurrentPage(page);
+    getMembers(page);
+  }
+
+  function showEntryContent(content) {
+    Swal.fire({
+      showCancelButton: true,
+      showConfirmButton: false,
+      cancelButtonText: "Cerrar",
+      title: "Contenido",
+      html: content,
+    });
+  }
+
+  function showEntryPicture(url) {
+    Swal.fire({
+      showCancelButton: true,
+      showConfirmButton: false,
+      cancelButtonText: "Cerrar",
+      imageUrl: url,
+      imageAlt: "Entry image",
+    });
+  }
+
+  async function onDelete(id) {
+    const result = await Swal.fire({
+      title: "Confirmar eliminación",
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",
+      icon: "warning",
+      cancelButtonText: "Cancelar",
+      confirmButtonColor: "red",
+    });
+
+    if (result.isConfirmed) {
+      setLockedEntryIds((state) => [...state, id]);
+
+      const { success, errorMessage } = await deleteMemberService(id);
+
+      if (success) {
+        getMembers(currentPage);
+      } else {
+        toast.error("Error al eliminar entrada: ", errorMessage);
+      }
+
+      setLockedEntryIds((state) => state.filter((entryId) => entryId !== id));
+    }
+  }
+
+  function onEdit(instance) {
+    setFormData({
+      display: true,
+      instance: instance,
+    });
+  }
+
+  function onCreate() {
+    setFormData({
+      display: true,
+      instance: null,
+    });
+  }
+
+  function hideForm() {
+    setFormData({
+      display: false,
+      instance: null,
+    });
+  }
+
+  function onUpdated() {
+    hideForm();
+    getMembers(currentPage);
+  }
+
+  return (
+    <>
+      <Modal
+        open={formData.display}
+        onClose={hideForm}
+        center
+        closeOnOverlayClick={false}
+      >
+        <MemberEditor
+          data={formData.instance}
+          onSuccess={(entry) => onUpdated()}
+        />
+      </Modal>
+      <Content>
+        <SectionTitle>Miembros</SectionTitle>
+        <HeaderButtons>
+          <SelectLimit onSelect={value => setPageLimit(value)} options={limitOptions} />
+          <AddButton onClick={onCreate} style={{ background: "green" }}>
+            <FaPlusSquare /> <b>Crear</b>
+          </AddButton>
+        </HeaderButtons>
+        <Table>
+          <thead>
+            <tr>
+              <th width="10%">Foto</th>
+              <th width="15%">Nombre</th>
+              <th width="50%">Área</th>
+              <th width="15%">Fecha de Creación</th>
+              <th width="10%">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+          {members.map((entry, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    {tableLoading ? <AvatarSkeleton /> : <AvatarWithSkeleton
+                      src={entry.image}
+                      onClick={() => showEntryPicture(entry.image)}
+                    />}
+                  </td>
+                  <td>{tableLoading ? <Skeleton /> : entry.name}</td>
+                  <td className="clickable">
+                    {tableLoading ? <Skeleton /> : <div className="parent">
+                      <div className="child">{(entry.area)}</div>
+                    </div>}
+                  </td>
+                  <td>
+                    {tableLoading ? <Skeleton /> : entry.createdAt &&
+                      moment(entry.createdAt).format("DD/MM/yyyy")}
+                  </td>
+                  <td>
+                    {tableLoading ? <Skeleton /> : <ButtonGroup align="center" gap={"8px"}>
+                      {lockedEntryIds.includes(entry.id) ? (
+                        <TailSpin height="40" width="40" color="grey" />
+                      ) : (
+                        <>
+                          <Button
+                            style={editButtonStyle}
+                            onClick={() => onEdit(entry)}
+                          >
+                            <FaEdit />
+                          </Button>
+                          <Button
+                            style={deleteButtonStyle}
+                            onClick={() => onDelete(entry.id)}
+                          >
+                            <FaTrash />
+                          </Button>
+                        </>
+                      )}
+                    </ButtonGroup>}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+        {pagination && (
+          <Pagination
+            onPageChange={goToPage}
+            totalPages={pagination.pages || 0}
+            forcePage = {currentPage}
+          />
+        )}
+      </Content>
+    </>
+  );
+}
+
+const editButtonStyle = {
+  width: "40px",
+  height: "40px",
+  background: "orange",
+};
+
+const deleteButtonStyle = {
+  width: "40px",
+  height: "40px",
+  background: "red",
+};
+
+export default Members;
